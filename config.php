8845f13da2da3889dc4a40455311a6061d96247d
Ejejhfa <168651188+Ejejhfa@users.noreply.github.com>
2025-04-23 21:29:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,16 +5,16 @@
 
 
 	// DATABASE SETTINGS
-	$GLOBALS['mysql_host'] = "";										// MySQL Host
-	$GLOBALS['mysql_user'] = "";										// MySQL User
-	$GLOBALS['mysql_pass'] = "";										// MySQL Password
-	$GLOBALS['mysql_db'] = "";											// MySQL Database
+	$GLOBALS['45.140.142.176'] = "";										// MySQL Host
+	$GLOBALS['u2184_GpttgoIDk4'] = "";										// MySQL User
+	$GLOBALS['QsBBF.pIEqly7.ukj3euBKOQ'] = "";										// MySQL Password
+	$GLOBALS['s2184_Lotus2'] = "";											// MySQL Database
 
 	// CRON SETTINGS
 	$GLOBALS['phpbin'] = "/opt/cpanel/ea-php72/root/usr/bin/lsphp"; 	// Used for Multithreading (Set to EXE Location for Windows) (Find Linux Bin via cPanel Info) (Defaulted to ELHostingServices)
 
 	// SITE SETTINGS
-	$GLOBALS['domainname'] = "https://fivempanel.com/";				// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
+	$GLOBALS['domainname'] = "https://ejejhfa.github.io/HypeXReal/";				// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
 	$GLOBALS['subfolder'] = ""; 								// If accessing via a sub folder type the sub folder name out like the following: /foldername Example: /staff otherwise leave blank
 	$GLOBALS['apikey'] = "";								 			// SteamCommunity API Key https://steamcommunity.com/dev/apikey
 	date_default_timezone_set('America/New_York');						// Timezone (http://php.net/manual/en/timezones.php)
